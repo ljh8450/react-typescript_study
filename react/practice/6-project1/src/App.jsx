80fd10256405ad6690ece668f0e4c2964e854cf7
BOOK-R4R6F38DIQ <dzigh30@gmail.com>
2026-04-06 19:34:11 +0900

    feat: View, Controller 구현 및 App 연동

--- react/practice/6-project1/src/App.jsx
+++ react/practice/6-project1/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
 import './App.css'
+import Controller from './component/Controller';
+import Viewer from './component/Viewer'; // Viewer 컴포넌트 로딩
 
 function App() {
-  return <div className='App'></div>;
+  const [count, setCount] = useState(0);
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1> {/* 제목 -> h1 */}
+      <section> {/* 섹션 분리, div와 동일 기능 */}
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount} />
+      </section>
+    </div>
+  );
 }
 
 export default App
